28f614f6e018fb776fb3fb58bd3cf86f18a8659a
Ralph Etienne <e.ralphdarly@gmail.com>
2024-12-24 00:59:58 -0600

    Initial Commit

---
+++ NooroWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NooroWeather
+//
+//  Created by Ralph Etienne on 12/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NooroWeatherTests/NooroWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherTests.swift
+//  NooroWeatherTests
+//
+//  Created by Ralph Etienne on 12/24/24.
+//
+
+import Testing
+@testable import NooroWeather
+
+struct NooroWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
